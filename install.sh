d76a71c1ef1bfae11c847ab5c4e800c1a89e2cce
root <4032563+trancongly@users.noreply.github.com>
2026-06-20 02:11:39 +0000

    add install

---
+++ install.sh
@@ -0,0 +1,22 @@
+proot-distro login debian
+root@localhost:~# history
+    2  apt update
+    3  apt install -y python3-pip python3-dev build-essential llvm
+    4  pip3 install numba pandas pandas-ta
+    5  apt update
+    6  apt install -y python3-venv python3-full
+    7  mkdir crypto_ai
+    8  cd crypto_ai/
+    9  python3 -m venv venv
+   10  source venv/bin/activate
+   11  pip3 install numba pandas pandas-ta
+   136  pip install -U google-genai
+  147  apt install -y cron
+  148  crontab -e
+  149  service cron start
+  151  crontab -l
+  152  pkg install termux-services
+  153  termux-wake-lock
+  165  apt install sqlite3
+  402  pip3 install numpy joblib
+  404  pip3 install scikit-learn
